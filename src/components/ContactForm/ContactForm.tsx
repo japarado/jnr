c431fb585e98ade14f989df7b342faa163f356ab
Justin Parado <justin.parado.personal@gmail.com>
2020-09-13 23:09:19 +0800

    Handlers and hooks for contact form component

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import "./ContactForm.scss";
 
@@ -11,6 +11,11 @@ const ContactForm = (): React.ReactElement =>
 	const [contactNumber, setContactNumber] = useState("");
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
+
+	useEffect(() => 
+	{
+		console.table({name, contactNumber, email, message});
+	}, [name, contactNumber, email, message]);
 
 	return(
 		<form className="ContactForm">
