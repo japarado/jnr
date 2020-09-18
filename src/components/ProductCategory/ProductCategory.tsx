63aa6c5bb02c39daeb15469c3c98e17a65556d4a
Justin Parado <justin.parado.personal@gmail.com>
2020-09-19 01:59:44 +0800

    Product category now receives slug instead of ID

--- src/components/ProductCategory/ProductCategory.tsx
+++ src/components/ProductCategory/ProductCategory.tsx
@@ -8,10 +8,11 @@ type Props = {
 	imgUrl: string;
 	text: string;
 	id: number | string;
+	slug: string;
 }
 
 const ProductCategory = (props: Props): React.ReactElement => (
-	<Link className="u-unstyled-list" to={`/products/${props.id}`}>
+	<Link className="u-unstyled-list" to={`/products/${props.slug}`}>
 		<div className="ProductCategory">
 			<img className="ProductCategory__image" src={props.imgUrl} alt={props.text} />
 			<p className="ProductCategory__text">{props.text}</p>
