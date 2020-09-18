c647e0a8ec874fbe4a82dc3395a6bc412187bad9
Justin Parado <justin.parado.personal@gmail.com>
2020-09-19 01:59:58 +0800

    Added hook-based handlers to the search field component

--- src/components/Navbar/SearchField/SearchField.tsx
+++ src/components/Navbar/SearchField/SearchField.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./SearchField.scss";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
-const SearchField = (): React.ReactElement => (
-	<div className="SearchField">
-		<FontAwesomeIcon icon={faSearch} className="SearchField__icon"/>
-		<input type="text" className="SearchField__input"/>
-	</div>
-);
+import {index} from "services/productCategories";
+
+type InputEvent = React.ChangeEvent<HTMLInputElement>;
+
+const SearchField = (): React.ReactElement => 
+{ 
+	const [value, setValue] = useState("");
+	const handleChange = (e: InputEvent): void => setValue(e.target.value);
+
+	return(
+		<div className="SearchField">
+			<FontAwesomeIcon icon={faSearch} className="SearchField__icon"/>
+			<input type="text" className="SearchField__input" value={value} onChange={handleChange}/>
+		</div>
+	);
+};
 
 
 export default SearchField;
