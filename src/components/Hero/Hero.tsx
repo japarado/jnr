82911dff7d10e14ede93264f60831d6655e841f7
Justin Parado <justin.parado.personal@gmail.com>
2020-09-19 01:58:09 +0800

    Hero component updates

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-import "./Hero.scss"
+import "./Hero.scss";
+import HeroText from "components/HeroText/HeroText";
 
 type Props = {
 	imgUrl: string;
 	heroText: string;
 }
 
-const Hero = ({imgUrl, heroText}: Props): React.ReactElement => (
+const Hero = (props: Props): React.ReactElement => (
 	<>
 		<div className="Hero">
-			<img className="Hero__image" src={imgUrl} alt="Hero" />
+			<div className="Hero__text">
+				<HeroText text={props.heroText}/>
+			</div>
+			<img className="Hero__image" src={props.imgUrl} alt="Hero" />
 		</div>
 	</>
 );
