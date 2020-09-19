1b85799c57a31902faec406823138bd5d8afe3a4
Justin Parado <justin.parado.personal@gmail.com>
2020-09-20 01:17:53 +0800

    Added a mock API setup for the search field

--- src/components/Navbar/SearchField/SearchField.tsx
+++ src/components/Navbar/SearchField/SearchField.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import "./SearchField.scss";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
-import {index} from "services/productCategories";
+import {searchByTerm} from "services/products";
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>;
 
@@ -13,6 +13,24 @@ const SearchField = (): React.ReactElement =>
 { 
 	const [value, setValue] = useState("");
 	const handleChange = (e: InputEvent): void => setValue(e.target.value);
+
+	useEffect(() => 
+	{
+		async function search(searchTerm: string): Promise<string[]> 
+		{
+			let response: string[] = [];
+			try 
+			{
+				response = await searchByTerm(searchTerm);
+			}
+			catch(error) 
+			{
+				console.log(error);
+			}
+			return response;
+		}
+		search(value);
+	}, [value]);
 
 	return(
 		<div className="SearchField">
