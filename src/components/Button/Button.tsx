12683ea6d1c38a50ebd54a22c09e85a767f74e42
Justin Parado <justin.parado.personal@gmail.com>
2020-09-22 00:04:08 +0800

    Formatting for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 
+import "./Button.scss";
+
 type Props = {
 	text?: string;
 	clickHandler?: Function;
 }
 
-const Button = (props: Props): React.ReactElement => (
-	<button className="Button">Click M</button>
-);
+type ClickEvent = React.MouseEvent<HTMLButtonElement>;
+
+const Button = (props: Props): React.ReactElement => 
+{
+	const handleClick = (e: ClickEvent) => props.clickHandler ? props.clickHandler() : (): void => 
+	{};
+	const text = props.text ? props.text : "Click Me";
+	return(
+		<button className="Button" onClick={handleClick}>{text}</button>
+	);
+};
+
+export default Button;
