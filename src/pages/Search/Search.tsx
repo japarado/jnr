7cbeccebed9554e1f473ef31ea1fd429586473b2
Justin Parado <justin.parado.personal@gmail.com>
2020-09-22 00:03:21 +0800

    Initial search page

---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = (): React.ReactElement => 
+{ 
+	return(
+		<div className="Search">
+			<div className="Search__params">
+			</div>
+		</div>
+	);
+};
+
+export default Search;
