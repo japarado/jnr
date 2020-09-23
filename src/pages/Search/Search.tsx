5e6fa7127617760ce355ab21aabda14c0f2d7225
Justin Parado <justin.parado.personal@gmail.com>
2020-09-24 02:02:07 +0800

    Handlers for search param change handlers

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./Search.scss";
 
 import ProductSearchBar from "components/ProductSearchBar/ProductSearchBar";
 import Products from "components/Products/Products";
 
+type Choice = {
+	label: string;
+	value: string;
+}
+
 const Search = (): React.ReactElement => 
 { 
+	const [brand, setBrand] = useState<Choice>();
+	const [category, setCategory] = useState<Choice>();
+	const [sortBy, setSortBy] = useState<Choice>();
+
 	return(
 		<div className="Search">
 			<div className="Search__section">
-				<ProductSearchBar/>
+				<ProductSearchBar 
+					handleUpdateBrand={setBrand}
+					handleUpdateCategory={setCategory}
+					handleUpdateSortBy={setSortBy}
+				/>
 			</div>
 
 			<div className="Search__section">
