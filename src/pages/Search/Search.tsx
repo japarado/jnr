f04793744e594bf0d171ece4eaec09e8fc3eb9a5
Justin Parado <justin.parado.personal@gmail.com>
2020-09-23 20:35:16 +0800

    Included product search bar in search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ProductSearchBar from "components/ProductSearchBar/ProductSearchBar";
 
 const Search = (): React.ReactElement => 
 { 
 	return(
 		<div className="Search">
-			<div className="Search__params">
+			<div className="Search__section">
+				<ProductSearchBar/>
 			</div>
 		</div>
 	);
