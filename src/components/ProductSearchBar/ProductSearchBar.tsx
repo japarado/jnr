67fbf3574e14d7ab29b28ff5d5dfbad916d3e002
Justin Parado <justin.parado.personal@gmail.com>
2020-09-23 23:12:10 +0800

    Removed hooks from product search bar component

--- src/components/ProductSearchBar/ProductSearchBar.tsx
+++ src/components/ProductSearchBar/ProductSearchBar.tsx
@@ -1,45 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
 import "./ProductSearchBar.scss";
 
 import Select from "react-select";
 
 type Choice = {
-	label?: string;
-	value?: string;
+	label: string;
+	value: string;
 }
 
-const ProductSearchBar = (): React.ReactElement => 
+type Props = {
+	brands?: Choice[];
+	categories?: Choice[];
+	sortBys?: Choice[];
+}
+
+const ProductSearchBar = (props: Props): React.ReactElement => 
 {
-	const [brand, setBrand] = useState({});
-	const [category, setCategory] = useState({});
-	const [sortBy, setSortBy] = useState({});
-
-	const brands: Choice[] = [];
-	const categories: Choice[] = [];
-	const sortBys: Choice[] = [];
-
-	useEffect(() => 
-	{
-		async function search(): Promise<Choice[]> 
-		{
-			return [];
-		}
-	});
-
 	return(
 		<div className="ProductSearchBar">
 			<div className="ProductSearchBar__pair">
 				<p className="ProductSearchBar__label">Brand</p>
-				<Select className="ProductSearchBar__select" options={brands}/>
+				<Select className="ProductSearchBar__select" placeholder="Brand" options={props.brands}/>
 			</div>
 			<div className="ProductSearchBar__pair">
 				<p className="ProductSearchBar__label">Category</p>
-				<Select className="ProductSearchBar__select" options={categories}/>
+				<Select className="ProductSearchBar__select" placeholder="Category" options={props.categories}/>
 			</div>
 			<div className="ProductSearchBar__pair">
 				<p className="ProductSearchBar__label">Sort By</p>
-				<Select className="ProductSearchBar__select" options={sortBys}/>
+				<Select className="ProductSearchBar__select" placeholder="Sort By" options={props.sortBys}/>
 			</div>
 		</div>
 	);
