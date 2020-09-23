080cd9d3e8d6c4c704c29222ebc392fe3ecf8be0
Justin Parado <justin.parado.personal@gmail.com>
2020-09-24 02:01:44 +0800

    Added type definition for Product component

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+
+type Props = {
+	name: string;
+	brand: string;
+	desc?: string;
+}
 
 const Product = ():  React.ReactElement => (
 	<h1>I am a product</h1>
-)
+);
 
 export default Product;
