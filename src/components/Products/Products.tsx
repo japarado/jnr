e8937b85ef222f34081c909e80bfa53e9ab48e55
Justin Parado <justin.parado.personal@gmail.com>
2020-09-23 23:11:28 +0800

    Created initial products list component

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Products.scss";
+import Product from "components/Product/Product";
+
+const Products = (): React.ReactElement => (
+	<div className="Products">
+		<Product/>
+	</div>
+);
+
+export 	default Products;
