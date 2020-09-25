9a480d22f08bc28eb76d40f67e79f2536e0ee3af
Justin Parado <justin.parado.personal@gmail.com>
2020-09-25 20:30:28 +0800

    Removed commented code

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -6,12 +6,6 @@ import {useParams} from "react-router-dom";
 
 import Hero from "components/Hero/Hero";
 
-/* import ACImage from "../../assets/img/categories/aircon.png"; */
-/* import TVImage from "../../assets/img/categories/tv.png"; */
-/* import CookingImage from "../../assets/img/categories/cooking.png"; */
-/* import ApplianceImage from "../../assets/img/categories/home_appliances.png"; */
-/* import MobileImage from "../../assets/img/categories/mobiles_gadgets.png"; */
-/* import BusinessImage from "../../assets/img/categories/business_machine.png"; */
 import CategoryLinks from "components/CategoryLinks/CategoryLinks";
 import ProductCategories from "components/ProductCategories/ProductCategories";
 
