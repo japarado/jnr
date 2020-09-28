2f1021dbc335c419a98eef92d1a1400953c73236
Justin Parado <justin.parado.personal@gmail.com>
2020-09-28 12:41:10 +0800

    Removed unused package

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -3,7 +3,6 @@ import React, {useState, useEffect} from "react";
 import "./Search.scss";
 
 import {useLocation, useHistory} from "react-router-dom";
-import qs from "qs";
 
 import ProductSearchBar from "components/ProductSearchBar/ProductSearchBar";
 import Products from "components/Products/Products";
