7e70a1dd56ba089e17ad611a10093c9a04757170
Justin Parado <justin.parado.personal@gmail.com>
2020-09-29 10:38:49 +0800

    Initial product component

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import Button from "components/Button/Button";
 
 type Props = {
+	id: number | string;
 	name: string;
 	brand: string;
 	desc?: string;
+	price?: number;
 }
 
 const Product = ():  React.ReactElement => (
-	<h1>I am a product</h1>
+	<div className="Product">
+		<img className="Product__img" src="" alt="" />
+		<h3 className="Product__name"></h3>
+		<p className="Product__price"></p>
+		<Button/>
+	</div>
 );
 
 export default Product;
