e4edb0e19f9fb9775a2725fb24a498c188cffb01
Justin Parado <justin.parado.personal@gmail.com>
2020-09-30 09:30:09 +0800

    Finished Product component implementation

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -2,22 +2,30 @@ import React from "react";
 
 import "./Product.scss";
 
+import {Product as ProductType} from "types";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
+
 import Button from "components/Button/Button";
 
-type Props = {
-	id: number | string;
-	name: string;
-	brand: string;
-	desc?: string;
-	price?: number;
-}
+type Props = ProductType;
 
-const Product = ():  React.ReactElement => (
+const Product = (props: Props):  React.ReactElement => (
 	<div className="Product">
-		<img className="Product__img" src="" alt="" />
-		<h3 className="Product__name"></h3>
-		<p className="Product__price"></p>
-		<Button className="Product__button"/>
+		<img
+			className="Product__img"
+			src={props.image}
+			alt={props.name} />
+		<p className="Product__brand">{props.brand}</p>
+		<h3 className="Product__name">{props.name}</h3>
+		<p className="Product__price">{props.currency} {props.price}</p>
+		<Button className="Product__button">
+			View More
+			<FontAwesomeIcon
+				icon={faChevronRight}
+				className="Product__chevron"/>
+		</Button>
 	</div>
 );
 
