e4a2e2bb22c072bcefccf64a1b9633e2a0f0529e
Justin Parado <justin.parado.personal@gmail.com>
2020-10-05 08:46:30 +0800

    Derived contact form field values from hooks

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -2,38 +2,77 @@ import React, {useState, useEffect} from "react";
 
 import "./ContactForm.scss";
 
+import {submit} from "services/contactForm";
+
 import TextInput from "components/FormElements/TextInput/TextInput";
 import TextArea from "components/FormElements/TextArea/TextArea";
 import Button from "components/Button/Button";
 
 const ContactForm = (): React.ReactElement => 
 { 
-	const [name, setName] = useState("");
-	const [contactNumber, setContactNumber] = useState("");
-	const [email, setEmail] = useState("");
-	const [message, setMessage] = useState("");
+	const [name, setName] = useState("Justin Parado");
+	const [contactNumber, setContactNumber] = useState("09122061571");
+	const [email, setEmail] = useState("justin.parado.personal@gmail.com");
+	const [message, setMessage] = useState("This is a nice message. Your website has a lot of room for improvement");
 
 	useEffect(() => 
 	{
 		/* console.table({name, contactNumber, email, message}); */
 	}, [name, contactNumber, email, message]);
 
+	function clearForm() 
+	{
+		setName("");
+		setContactNumber("");
+		setEmail("");
+		setMessage("");
+	}
+
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> 
+	{
+		e.preventDefault();
+		await submit({name, contactNumber, email, message});
+		clearForm();
+	}
+
+
 	return(
-		<form className="ContactForm">
+		<form
+			className="ContactForm"
+			onSubmit={handleSubmit}>
 			<div className="ContactForm__form-group">
-				<TextInput name="name" placeholder="Name" changeHandler={setName}/>
+				<TextInput
+					name="name"
+					placeholder="Name"
+					value={name}
+					changeHandler={setName}/>
 			</div>
 
 			<div className="ContactForm__form-group">
-				<TextInput name="contact-number" placeholder="Contact Number" changeHandler={setContactNumber}/>
+				<TextInput
+					name="contact-number"
+					placeholder="Contact Number"
+					value={contactNumber}
+					changeHandler={setContactNumber}/>
 			</div>
 
 			<div className="ContactForm__form-group">
-				<TextInput name="email" type="email" placeholder="Email" changeHandler={setEmail}/>
+				<TextInput
+					name="email"
+					type="email"
+					placeholder="Email"
+					value={email}
+					changeHandler={setEmail}/>
 			</div>
 
-			<div className="ContactForm__form-group" placeholder="Message">
-				<TextArea name="message" placeholder="Message" changeHandler={setMessage}/>
+			<div
+				className="ContactForm__form-group"
+				placeholder="Message">
+				<TextArea
+					name="message"
+					placeholder="Message"
+					value={message}
+					changeHandler={setMessage}/>
 			</div>
 
 			<div className="ContactForm__button">
